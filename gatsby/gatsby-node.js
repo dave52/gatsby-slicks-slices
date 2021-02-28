3d1a4a8f7a44805f5d0e99dd55a39c427a7b75d9
Hudson, David <david.hudson@zywave.com>
2021-02-27 19:54:09 -0600

    feat: add slicemasters w pagination and sngl view

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -95,6 +95,55 @@ async function fetchBeersAndTurnIntoNodes({
   }
 }
 
+async function turnSlicemastersIntoPages({graphql, actions}) {
+  // 1. query all slicemasters
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // TODO: 2. turn each slicemaster into their own page
+  data.slicemasters.nodes.forEach(slicemaster => {
+    actions.createPage({
+      component: resolve('./src/templates/Slicemaster.js'),
+      path: `/slicemasters/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current
+      }
+    })
+  })
+  // 3. figure out how many pages there are based on how many slicemasters there are, an dhow many per page
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+  console.log(`There are ${data.slicemasters.totalCount} and we have ${pageCount} pages with ${pageSize} per page`);
+  // 4. loop from 1 to n and create the pages for them
+  // Array.from({ length: pageCount }).forEach(_, i) => console.log(i);
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    console.log(`Creating page ${i}`);
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      // this data is passed to the template when we create it
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+
+    });
+  });
+}
+
 export async function sourceNodes(params) {
   // fetch a list of beers and source them into our gatsby API
   await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
@@ -106,6 +155,7 @@ export async function createPages(params) {
   await Promise.all([
     turnPizzasIntoPages(params),
     turnToppingsIntoPages(params),
+    turnSlicemastersIntoPages(params),
   ]);
   // 1. pizzas
   // await turnPizzasIntoPages(params);
